d6124bd7fe33aa7683718d3861d201ae357063d7
Smartfeld.ch <78407877+smartfeld-ch@users.noreply.github.com>
2021-07-19 14:51:12 +0200

    Pegelmessung mit Maximalwert

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    led.stopAnimation()
+    basic.clearScreen()
+    basic.showNumber(max_lautstärke)
+    basic.pause(1000)
+})
+input.onButtonPressed(Button.B, function () {
+    max_lautstärke = 0
+})
+let max_lautstärke = 0
+basic.showString(":)")
+let schallpegel = input.soundLevel()
 basic.forever(function () {
-	
+    schallpegel = input.soundLevel()
+    if (schallpegel > 150) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    }
+    if (schallpegel > max_lautstärke) {
+        max_lautstärke = schallpegel
+    }
 })
